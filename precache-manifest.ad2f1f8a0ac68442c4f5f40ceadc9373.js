939fe96ba0d20ed3c6f8b532c1b46f597e494357
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-10 21:26:40 +0200

    Updates

---
+++ precache-manifest.ad2f1f8a0ac68442c4f5f40ceadc9373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d77ac29956170ea9e1fba7c797f941b",
+    "url": "/tours-project/index.html"
+  },
+  {
+    "revision": "29715d3b58417eb914c1",
+    "url": "/tours-project/static/css/2.b5e82d2c.chunk.css"
+  },
+  {
+    "revision": "3a1d4174e104f6469aa9",
+    "url": "/tours-project/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "29715d3b58417eb914c1",
+    "url": "/tours-project/static/js/2.ada9a067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours-project/static/js/2.ada9a067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1d4174e104f6469aa9",
+    "url": "/tours-project/static/js/main.49391bd4.chunk.js"
+  },
+  {
+    "revision": "dbdfe489273ec43c1716",
+    "url": "/tours-project/static/js/runtime-main.0f695b42.js"
+  }
+]);
